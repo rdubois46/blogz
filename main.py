922957f47500ad6c235603fda201930fd3dcdeee
Ryan DuBois <ryan-dubois@utulsa.edu>
2019-04-28 16:17:39 -0500

    cosmetic tweaks + changed code to display username when logged in. 'Logout' only displays if user currently logged in.

--- main.py
+++ main.py
@@ -33,30 +33,44 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+def logged_in():
+    if 'user' in session:
+        return User.query.filter_by(username=session['user']).first()
+    else:
+        return False
 
 @app.route("/blog", methods=['POST', 'GET'])
 def blog():
+    current_user = None
+    if logged_in():
+        current_user = logged_in()   
+
     post_id = request.args.get("id")
     single_post = False
 
     if post_id:
         posts = [Post.query.filter_by(id=int(post_id)).first()]
         single_post = True
-        return render_template("blog.html", posts=posts, single_post=single_post)
+        return render_template("blog.html", posts=posts, single_post=single_post, current_user=current_user)
     
     user_id = request.args.get("user")
 
     if user_id:
         user_posts = Post.query.filter_by(author_id=user_id).all()
         user = User.query.filter_by(id=user_id).first()
-        return render_template("singleuser.html", user_posts=user_posts, user=user)
+        return render_template("singleuser.html", user_posts=user_posts, user=user, current_user=current_user)
 
     posts = Post.query.all()
-    return render_template("blog.html", posts=posts, single_post=single_post)    
+    return render_template("blog.html", posts=posts, single_post=single_post, current_user=current_user)    
     
 
 @app.route("/newpost", methods=['POST', 'GET'])
 def new_post():
+    current_user = None
+    if logged_in():
+        current_user = logged_in()
+
+
     if request.method == "POST":
 
         title = request.form['title']
@@ -76,7 +90,7 @@ def new_post():
 
         if error:
             return render_template("newpost.html", title=title, content=content, 
-                title_error=title_error, content_error=content_error)
+                title_error=title_error, content_error=content_error, current_user=current_user)
 
         author = User.query.filter_by(username=session['user']).first()
         post = Post(title, content, author)
@@ -85,11 +99,16 @@ def new_post():
 
         return redirect("/blog?id=" + str(post.id))
     else:
-        return render_template('newpost.html')
+        return render_template('newpost.html', current_user=current_user)
 
 
 @app.route('/signup', methods = ['GET', 'POST'])
+
 def signup():
+    current_user = None
+    if logged_in():
+        current_user = logged_in()
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -97,15 +116,19 @@ def signup():
 
         if not username or not 3 < len(username) < 20:
             flash("You must enter a name between 3 and 20 characters.", "error")
-            return render_template('signup.html')
+            return render_template('signup.html', current_user=current_user)
+
+        if User.query.filter_by(username=username).first():
+            flash("That username is already taken.", "error")
+            return render_template('signup.html', current_user=current_user)
         
         if not password or not 3 < len(password) < 20: 
             flash("You must enter a password between 3 and 20 characters.", "error")
-            return render_template('signup.html', username=username)
+            return render_template('signup.html', username=username, current_user=current_user)
 
         if password != verify:
             flash("Those passwords do not match.", "error")
-            return render_template('signup.html', username=username)
+            return render_template('signup.html', username=username, current_user=current_user)
 
         new_user = User(username, password)
         db.session.add(new_user)
@@ -114,17 +137,15 @@ def signup():
         flash("Signup Success! Welcome!", "success")
         return redirect('/newpost')
     else:
-        return render_template('signup.html')
+        return render_template('signup.html', current_user=current_user)
         
         
-
-
-
-
-
-
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    current_user = None
+    if logged_in():
+        current_user = logged_in()
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -133,24 +154,28 @@ def login():
 
         if not user:
             flash('That user does not exist!', 'error')
-            return render_template('login.html')
+            return render_template('login.html', current_user=current_user)
 
         if password != user.password:
             flash('Incorrect password!', 'error')
-            return render_template('login.html', username=username)
+            return render_template('login.html', username=username, current_user=current_user)
 
         else:
             session['user'] = username
             flash("Login Success!", 'success')            
             return redirect('/newpost')
     else:
-        return render_template('login.html')
+        return render_template('login.html', current_user=current_user)
             
     
 
 
 @app.route('/')
 def index():
+    current_user = None
+    if logged_in():
+        current_user = logged_in()
+
     users = User.query.all()
 
     #user_names = []
@@ -158,7 +183,7 @@ def index():
     #for user in users:
         #user_names.append(user.username)
     
-    return render_template('index.html', users=users)
+    return render_template('index.html', users=users, current_user=current_user)
 
 
 @app.route('/logout')
